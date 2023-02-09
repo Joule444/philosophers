ec8f8b43fdb5e1bce1f01cfc8ab23e11bbf1e4bd
Jules <julesthuys@gmail.com>
2023-02-09 14:53:35 +0100

    Timestamp order fixed

--- philo/srcs/observer.c
+++ philo/srcs/observer.c
@@ -6,7 +6,7 @@
 /*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 14:24:06 by jules             #+#    #+#             */
-/*   Updated: 2023/02/07 18:34:40 by jules            ###   ########.fr       */
+/*   Updated: 2023/02/09 14:51:22 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,22 @@ void	*observer(void *param)
 	philo = param;
 	while (philo->data->observer->end == 0)
 	{
-		pthread_mutex_lock(&philo->data->micro);
+		// pthread_mutex_lock(&philo->data->micro);
 		i = 0;
 		while (i < philo->data->nb_philo && philo->data->observer->end == 0)
 		{
 			// printf("time=%ld\nlast_meal=%ld\nlast_meal - time=%ld\nttd=%ld\n", time, philo[i].last_meal, time - philo[i].last_meal, philo->data->ttd);
 			if (is_dead(philo[i]))
 			{
-				pthread_mutex_unlock(&philo->data->micro);
-				print_state(philo[i], DIED);
-				pthread_mutex_lock(&philo->data->micro);
+				// pthread_mutex_unlock(&philo->data->micro);
+				// print_state(philo[i], DIED);
+				printf("%d is dead\n", i + 1);
+				// pthread_mutex_lock(&philo->data->micro);
 				philo->data->observer->end = 1;
 			}
 			i++;
 		}
-		pthread_mutex_unlock(&philo->data->micro);
+		// pthread_mutex_unlock(&philo->data->micro);
 		usleep(700);
 	}
 	return (NULL);
